83e37ab2823b637e53abb465df0f523a76c26f65
Adam Cessna <racessna@gmail.com>
2020-01-19 17:26:30 -0600

    Add global variables

--- js/script.js
+++ js/script.js
@@ -7,17 +7,12 @@ FSJS project 2 - List Filter and Pagination
 
 
 /*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
+Global variables that hold all the students and tell the number of students that should be on each page.
 ***/
+let listItems = document.querySelector('.student-list').children;
+const numOfPageItems = 10;
 
-
+console.log(listItems[0]);
 
 
 /*** 
@@ -35,6 +30,10 @@ FSJS project 2 - List Filter and Pagination
        "invoke" the function 
 ***/
 
+function showPage (listItems) {
+
+
+}
 
 
 
